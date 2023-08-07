5dd9493bcc74ad1e51b96cbfa88a6bed35626071
Francisco Motta <motta.lopes@hotmail.com>
2023-08-07 00:47:32 -0300

    slick lity ok

--- VRDRONE/assets/js/app.js
+++ VRDRONE/assets/js/app.js
@@ -1,5 +1,15 @@
 $('.banner-slick').slick({
     lazyLoad: 'ondemand',
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true
+});
+
+$('.video-slick').slick({
+    lazyLoad: 'ondemand',
+    infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
