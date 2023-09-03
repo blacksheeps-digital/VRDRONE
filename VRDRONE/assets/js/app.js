e85d1b594efabd35f609a97aa7f4d0fc116b48ab
Francisco Motta <motta.lopes@hotmail.com>
2023-09-03 05:24:17 -0300

    matter port dando pau

--- VRDRONE/assets/js/app.js
+++ VRDRONE/assets/js/app.js
@@ -1,3 +1,25 @@
+// // Navbar Scroll
+const navbar = document.querySelector('.navbar');
+const navbarHeightPx = 300; // Adjust this value as needed
+
+function addClassOnScroll() {
+    navbar.classList.add('fixed-top');
+}
+
+function removeClassOnScroll() {
+    navbar.classList.remove('fixed-top');
+}
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeightPx) {
+        navbar.style.backgroundColor = '#192647';
+        addClassOnScroll();
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+        removeClassOnScroll();
+    }
+});
+
 $('.banner-slick').slick({
     lazyLoad: 'ondemand',
     infinite: false,
