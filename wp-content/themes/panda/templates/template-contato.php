604648b1eaa70a28a88e5b749e79dfb7bae53f6e
Francisco Motta <motta.lopes@hotmail.com>
2023-09-11 22:55:24 -0300

    contato e onde estamos

--- wp-content/themes/panda/templates/template-contato.php
+++ wp-content/themes/panda/templates/template-contato.php
@@ -2,46 +2,78 @@
 /*
 Template Name: Contato
 */
-get_header( );
+get_header();
 ?>
 
+<section class="contato">
+	<div class="container h-100">
+		<div class="row align-items-start justify-content-center h-100">
+			<div class="col-md-2"></div>
+			<div class="col-md-4 text-start text-md-end">
+				<h1 class="huge black mb-3 mb-md-5 fw-bold">
+					<?php the_field('vamos_conversar'); ?>
+				</h1>
+				<p class="mb-4 mb-md-0">
+					<?php the_field('texto'); ?>
+				</p>
+				<div class="col-md-10 col-xl-7 ms-md-auto mt-md-5 pt-md-5 mb-5 mb-md-0">
+					<div class="d-flex flex-row align-items-center justify-content-between socials col-md-12 p-0">
 
-<h1>Contato</h1>
+
+						<?php if (have_rows('social_media', 'option')) : ?>
+							<?php while (have_rows('social_media', 'option')) : the_row(); ?>
+								<?php $icon = get_sub_field('icon'); ?>
+								<a href="">
+									<i>
+										<?php if ($icon) : ?>
+											<img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>" />
+											<!-- <img src='./assets/images/facebook.png' class='img-fluid' alt='' title='' loading='lazy'> -->
+										<?php endif; ?>
+									</i>
+								</a>
+								<!-- <?php the_sub_field('name'); ?>
+								<?php the_sub_field('link'); ?> -->
+							<?php endwhile; ?>
+						<?php else : ?>
+							<?php // No rows found 
+							?>
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+			<div class="col-md-5">
+
+				<?php echo do_shortcode('[contact-form-7 id="74" title="Formulário de Contato"]'); ?>
+
+			</div>
+		</div>
+	</div>
+	</div>
+</section>
 
 
-<?php echo do_shortcode('[contact-form-7 id="74" title="Formulário de Contato"]');?>
 
- 
-<?php if ( have_rows( 'social_media', 'option' ) ) : ?>
-	<?php while ( have_rows( 'social_media', 'option' ) ) : the_row(); ?>
-		<?php $icon = get_sub_field( 'icon' ); ?>
-		<?php if ( $icon ) : ?>
-			<img src="<?php echo esc_url( $icon['url'] ); ?>" alt="<?php echo esc_attr( $icon['alt'] ); ?>" />
-		<?php endif; ?>
-		<?php the_sub_field( 'name' ); ?>
-		<?php the_sub_field( 'link' ); ?>
+
+<!-- 
+<?php if (have_rows('location', 'option')) : ?>
+	<?php while (have_rows('location', 'option')) : the_row(); ?>
+		<?php the_sub_field('city'); ?>
+		<?php the_sub_field('country'); ?>
 	<?php endwhile; ?>
 <?php else : ?>
-	<?php // No rows found ?>
+	<?php // No rows found 
+	?>
 <?php endif; ?>
-<?php if ( have_rows( 'location', 'option' ) ) : ?>
-	<?php while ( have_rows( 'location', 'option' ) ) : the_row(); ?>
-		<?php the_sub_field( 'city' ); ?>
-		<?php the_sub_field( 'country' ); ?>
+<?php if (have_rows('phones', 'option')) : ?>
+	<?php while (have_rows('phones', 'option')) : the_row(); ?>
+		<?php the_sub_field('phone_number'); ?>
 	<?php endwhile; ?>
 <?php else : ?>
-	<?php // No rows found ?>
-<?php endif; ?>
-<?php if ( have_rows( 'phones', 'option' ) ) : ?>
-	<?php while ( have_rows( 'phones', 'option' ) ) : the_row(); ?>
-		<?php the_sub_field( 'phone_number' ); ?>
-	<?php endwhile; ?>
-<?php else : ?>
-	<?php // No rows found ?>
-<?php endif; ?>
-<?php the_field( 'vamos_conversar' ); ?>
-<?php the_field( 'texto' ); ?>
+	<?php // No rows found 
+	?>
+<?php endif; ?> -->
 
-<?php 
+<?php
 get_footer();
 ?>
